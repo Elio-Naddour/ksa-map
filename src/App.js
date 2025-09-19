2a64954c452abadc4697333251ee2ff0b7b0ad78
nzeidan <egnaddour@netways.com>
2025-09-19 20:41:41 +0300

    sawt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import SaudiMapButtons from './components/mapSvg';
 import logos from './images/logos.png'
+import sawt from './images/sawt.png'
+
 function App() {
   return (
       <div className='map-container'>
         <div className='header'>
           <img src={logos}/>
+          <img className='sawt' src={sawt}/>
           <text>إسمع صوت الوطن</text>
         </div>
         <SaudiMapButtons />
